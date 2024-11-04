2ad84bcb757f61c22cd5d89dd2482dec5e5fea3e
emwangangi <emwangangi@jambopay.com>
2024-11-04 16:04:05 +0300

    initial commit

---
+++ data/index.ts
@@ -0,0 +1,252 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+    width: 100,
+    height: 100,
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+    width: 100,
+    height: 100,
+  },
+
+  {
+    id: 5,
+    title: "Currently building a revenue collections system",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/sisibo.png",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+    width: 100,
+    height: 100,
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Sisibopay E-service Portal",
+    des: "The ultimate goal of every government is to make essential services easily accessible to all citizens. With the Sisibopay e-Service Portal, citizens can now apply for and make payments for government services conveniently from their smartphones and other devices. This portal streamlines access to government services, bringing them directly to the user, enhancing efficiency and accessibility",
+    img: "/sisibo.png",
+    iconLists: [
+      "angular.png",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "angularMaterial.png",
+    ],
+    link: "https://sisibopay.uasingishu.go.ke/#/",
+  },
+  {
+    id: 2,
+    title: "Ride-Share dashboard",
+    des: "A user-friendly dashboard  designed to track customer trips, earnings, drivers, onboarding processes, transactions, reports, and more essential features.",
+    img: "taxi.png",
+    iconLists: [
+      "angular.png",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "angularMaterial.png",
+    ],
+    link: "http://3.79.111.229/#/login",
+  },
+  {
+    id: 3,
+    title: "KimPay Auth",
+    des: "A unified authentication solution, such as Google Auth, enables seamless login across multiple applications. Users are redirected to a centralized login page, then automatically directed back to the original site, streamlining access across the platform.",
+    img: "kimPay.png",
+    iconLists: [
+      "angular.png",
+      "/tail.svg",
+      "/ts.svg",
+      "/three.svg",
+      "angularMaterial.png",
+    ],
+    link: "https://auth-ui-pink.vercel.app/login",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Evans at THEJITU was an incredibly valuable experience. As an instructor, Evans was part of my team of  developers, and he consistently demonstrated eagerness to learn and a proactive approach during sessions. He completed all tasks on time and actively sought clarification whenever needed, ensuring his full understanding of each concept. His determination and engagement placed him among the top performers on our team. I would highly recommend Evans to any employer.",
+    name: "Jonathan Ndambuki",
+    title: "Software Developer || Instructor || Mentor at THEJITU",
+    img: "/jonathan.png",
+    link: "https://www.linkedin.com/in/jonathan-ndambuki-84630a219/",
+    socialIcon: "/link.svg",
+  },
+  {
+    quote:
+      "Collaborating with Evans was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Evans's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Evans is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+    img: "/jonathan.png",
+    link: "/jonathan.png",
+    socialIcon: "/link.svg",
+  },
+  {
+    quote:
+      "Collaborating with Evans was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Evans's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Evans is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+    img: "/jonathan.png",
+    link: "/jonathan.png",
+    socialIcon: "/link.svg",
+  },
+  {
+    quote:
+      "Collaborating with Evans was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Evans's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Evans is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+    img: "/jonathan.png",
+    link: "/jonathan.png",
+    socialIcon: "/link.svg",
+  },
+  {
+    quote:
+      "Collaborating with Evans was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Evans's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Evans is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+    img: "/jonathan.png",
+    link: "/jonathan.png",
+    socialIcon: "/link.svg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Software Engineer Intern",
+    desc: "Assisted in designing the database structure and developing an HR portal with PHP and C#. Initially, I handled smaller tasks, progressively taking on more complex responsibilities as I gained experience, ultimately contributing to key areas of the project.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Junior Frontend developer",
+    desc: "Developed and maintained user-facing features using Angular.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: " Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies. Led the seamless integration of a customized steps-bot with the company’s primary platform, enhancing efficiency and user experience. Collaborated closely with cross-functional teams to analyze requirements, design workflows, and implement robust API integrations.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: " Mobile Developer",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using Angular ionic.",
+
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/mwangangievans",
+  },
+
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/evans-mwangangi-00ab3717a/",
+  },
+];
